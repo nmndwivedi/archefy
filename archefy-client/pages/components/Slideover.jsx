baceb988bd5a0ff40b785090664ee39299822c30
Naman <nmndwivedi@gmail.com>
2022-04-23 00:55:33 +0200

    login buttons

--- archefy-client/pages/components/Slideover.jsx
+++ archefy-client/pages/components/Slideover.jsx
@@ -3,6 +3,7 @@ import { Dialog, Menu } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 import { DotsVerticalIcon } from "@heroicons/react/solid";
 import { PencilIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -10,6 +11,8 @@ function classNames(...classes) {
 
 //Login Details, Logout Button, Address, Order History Button, Investment History Button || Login with...
 export default function Slideover({ open, setOpen }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Dialog
       as="div"
@@ -29,12 +32,12 @@ export default function Slideover({ open, setOpen }) {
                     id="slide-over-heading"
                     className="text-lg font-medium text-gray-900"
                   >
-                    My Information
+                    {isLoggedIn ? "My Information" : "Please login"}
                   </h2>
                   <div className="ml-3 flex h-7 items-center">
                     <button
                       type="button"
-                      className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500"
+                      className="rounded-md bg-white text-gray-400 hover:text-gray-700"
                       onClick={() => setOpen(false)}
                     >
                       <span className="sr-only">Close panel</span>
@@ -44,42 +47,44 @@ export default function Slideover({ open, setOpen }) {
                 </div>
               </div>
               {/* Main */}
-              <div className="pb-1 sm:pb-6">
-                <div>
-                  <div className="mt-6 px-4 sm:mt-0 sm:flex sm:items-end sm:px-6">
-                    <div className="sm:flex-1">
-                      <div>
-                        <div className="flex items-center">
-                          <h3 className="text-xl font-bold text-gray-900 sm:text-2xl">
-                            Ashley Porter
-                          </h3>
-                          <span className="ml-2.5 inline-block h-2 w-2 flex-shrink-0 rounded-full bg-green-400">
-                            <span className="sr-only">Online</span>
-                          </span>
-                        </div>
-                        <p className="text-sm text-gray-500">
-                          ashleyporter@gmail.com
-                        </p>
-                      </div>
-                      <div className="mt-5 flex flex-wrap space-y-3 sm:space-y-0 sm:space-x-3">
-                        <button
-                          type="button"
-                          className="inline-flex w-full flex-shrink-0 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:flex-1"
-                        >
-                          My Orders
-                        </button>
-                        <button
-                          type="button"
-                          className="inline-flex w-full flex-1 items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                          My Investments
-                        </button>
-                        {/* <span className="ml-3 inline-flex sm:ml-0">
+              {isLoggedIn && (
+                <>
+                  <div className="pb-1 sm:pb-6">
+                    <div>
+                      <div className="mt-6 px-4 sm:mt-0 sm:flex sm:items-end sm:px-6">
+                        <div className="sm:flex-1">
+                          <div>
+                            <div className="flex items-center">
+                              <h3 className="text-xl font-bold text-gray-900 sm:text-2xl">
+                                Ashley Porter
+                              </h3>
+                              <span className="ml-2.5 inline-block h-2 w-2 flex-shrink-0 rounded-full bg-green-400">
+                                <span className="sr-only">Online</span>
+                              </span>
+                            </div>
+                            <p className="text-sm text-gray-500">
+                              ashleyporter@gmail.com
+                            </p>
+                          </div>
+                          <div className="mt-5 flex flex-wrap space-y-3 sm:space-y-0 sm:space-x-3">
+                            <button
+                              type="button"
+                              className="inline-flex w-full flex-shrink-0 items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:flex-1"
+                            >
+                              My Orders
+                            </button>
+                            <button
+                              type="button"
+                              className="inline-flex w-full flex-1 items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            >
+                              My Investments
+                            </button>
+                            {/* <span className="ml-3 inline-flex sm:ml-0">
                           <Menu
                             as="div"
                             className="relative inline-block text-left"
                           >
-                            <Menu.Button className="inline-flex items-center rounded-md border border-gray-300 bg-white p-2 text-sm font-medium text-gray-400 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            <Menu.Button className="inline-flex items-center rounded-md border border-gray-300 bg-white p-2 text-sm font-medium text-gray-400 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                               <span className="sr-only">Open options menu</span>
                               <DotsVerticalIcon
                                 className="h-5 w-5"
@@ -122,43 +127,70 @@ export default function Slideover({ open, setOpen }) {
                             </Menu.Items>
                           </Menu>
                         </span> */}
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <div className="px-4 pt-5 pb-5 sm:px-0 sm:pt-0">
+                    <dl className="space-y-8 px-4 sm:space-y-6 sm:px-6">
+                      <div>
+                        <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 flex justify-start items-center space-x-2">
+                          <span>Delivery Address:</span>
+                          <PencilIcon className="w-5 h-5 cursor-pointer" />
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                          <p>72/1 Westerpark</p>
+                          <p>Amsterdam</p>
+                          <p>1051RA</p>
+                          <p>The Netherlands</p>
+                        </dd>
+                      </div>
+
+                      <button
+                        type="button"
+                        className="inline-flex w-32 flex-shrink-0 items-center justify-center rounded-md border border-transparent  border-red-600 px-4 py-2 text-sm font-medium text-red-600 shadow-sm hover:bg-red-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:flex-1"
+                      >
+                        Logout
+                      </button>
+                    </dl>
+                  </div>
+                </>
+              )}
+              {!isLoggedIn && (
+                <div className="pb-1 sm:pb-6">
+                  <div className="mt-6 px-4 sm:mt-8 sm:flex sm:items-start sm:px-6 flex flex-col">
+                    <span>Use one of these services</span>
+                    <button className="mt-2 w-48 rounded-full flex items-center space-x-1 border border-blue-500 font-medium">
+                      <img src="/logos/fb.svg" alt="fb" className="w-8 h-8 m-1" />
+                      <p>Facebook</p>
+                    </button>
+                    <button className="mt-2 w-48 rounded-full flex items-center space-x-1 border border-red-500 font-medium">
+                      <img src="/logos/go.svg" alt="go" className="w-10 h-10" />
+                      <p>Google</p>
+                    </button>
+                    <button className="mt-2 w-48 rounded-full flex items-center space-x-1 border border-blue-400 font-medium">
+                      <img src="/logos/tw.svg" alt="tw" className="w-6 h-6 m-2" />
+                      <p>Twitter</p>
+                    </button>
+                  </div>
+                  <div className="mt-4 px-4 sm:mt-6 sm:flex sm:items-start sm:px-6 flex flex-col">
+                    <span>Or, securely login with your crypto wallet</span>
+                    <button className="mt-2 w-48 rounded-full flex items-center space-x-1 border border-orange-500 font-medium">
+                      <img src="/logos/mm.svg" alt="fb" className="w-6 h-6 m-2" />
+                      <p>MetaMask</p>
+                    </button>
+                    <button className="mt-2 w-48 rounded-full flex items-center space-x-1 border border-blue-600 font-medium">
+                      <img src="/logos/wc.svg" alt="go" className="w-6 h-6 m-2" />
+                      <p>WalletConnect</p>
+                    </button>
+                    <button className="mt-2 w-48 rounded-full flex items-center space-x-1 border border-blue-700 font-medium">
+                      <img src="/logos/cb.png" alt="tw" className="w-6 h-6 m-2" />
+                      <p>Coinbase Wallet</p>
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <div className="px-4 pt-5 pb-5 sm:px-0 sm:pt-0">
-                <dl className="space-y-8 px-4 sm:space-y-6 sm:px-6">
-                  <div>
-                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 flex justify-start items-center space-x-2">
-                      <span>Delivery Address:</span>
-                      <PencilIcon className="w-5 h-5 cursor-pointer" />
-                    </dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                      <p>72/1 Westerpark</p>
-                      <p>Amsterdam</p>
-                      <p>1051RA</p>
-                      <p>The Netherlands</p>
-                    </dd>
-                  </div>
-                  <div>
-                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
-                      Website
-                    </dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                      ashleyporter.com
-                    </dd>
-                  </div>
-                  <div>
-                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
-                      Birthday
-                    </dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                      <time dateTime="1988-06-23"> June 23, 1988 </time>
-                    </dd>
-                  </div>
-                </dl>
-              </div>
+              )}
             </div>
           </div>
         </div>
